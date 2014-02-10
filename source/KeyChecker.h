18722ae79873795a4aa2d218b0f3669d2e45e808
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-10 10:53:26 +0400

    [Mod]: Косметические изменения порядка строк

--- source/KeyChecker.h
+++ source/KeyChecker.h
@@ -21,13 +21,13 @@ class KeyChecker : public IKeyChecker, public ICheckMethodsManager, public IChec
 	protected:
 		virtual void set_max_check_number(check_number_t check_number) const final;
 		virtual void logout(void) const;
-	private:
-		mutable std::vector<std::shared_ptr<CheckMethod>> m_check_methods;
-		std::map<std::shared_ptr<CheckMethodLogin>, handle_t> m_handles;
 
 		/* IKeyChecker Interface */
 		virtual const bool is_base_key_available(const std::shared_ptr<const CheckMethodBase> checkMethod) const override = 0;
 		virtual const bool is_able_to_login(const std::shared_ptr<const CheckMethodLogin> checkMethod) const override;
 		virtual const bool is_same_memory(const std::shared_ptr<const CheckMethodMemory> checkMethod) const override = 0;
 		virtual const bool logout_key(const std::shared_ptr<const CheckMethodLogin> checkMethod) const override = 0;
+	private:
+		mutable std::vector<std::shared_ptr<CheckMethod>> m_check_methods;
+		std::map<std::shared_ptr<CheckMethodLogin>, handle_t> m_handles;
 };
