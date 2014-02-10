8e69c359f9ec6d9a14a12e34dd0d7cb13b85dc2f
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-10 16:27:24 +0400

    [Ref]: Тип контейнера вынесен в using

--- source/KeyChecker.h
+++ source/KeyChecker.h
@@ -10,6 +10,9 @@
 #include "ICheckMethodsViewer.h"
 #include "CheckMethod.h"
 
+using handles_t = std::map<const std::shared_ptr<const CheckMethodLogin>, const handle_t>;
+using check_methods_t = std::vector<std::shared_ptr<const CheckMethod>>;
+
 class KeyChecker : public IKeyChecker, public ICheckMethodsManager, public ICheckMethodsViewer {
 	public:
 		KeyChecker(void);
@@ -19,7 +22,7 @@ class KeyChecker : public IKeyChecker, public ICheckMethodsManager, public IChec
 		const std::shared_ptr<const CheckMethod> create_check_method_login(const feature_t feature, const bool allow_to_login_on_previous_key) const;
 		const std::shared_ptr<const CheckMethod> create_check_method_memory(const offset_t offset, std::vector<const byte_t>& value, std::shared_ptr<const CheckMethod> logged_in_method) const;
 	protected:
-		mutable std::vector<std::shared_ptr<const CheckMethod>> m_check_methods;
+		mutable check_methods_t m_check_methods;
 
 		virtual const bool check(void) const = 0;
 		virtual void set_max_check_number(check_number_t check_number) const final;
@@ -31,5 +34,5 @@ class KeyChecker : public IKeyChecker, public ICheckMethodsManager, public IChec
 		virtual const bool is_same_memory(const std::shared_ptr<const CheckMethodMemory> checkMethod) const override = 0;
 		virtual const bool logout_key(const std::shared_ptr<const CheckMethodLogin> checkMethod) const override = 0;
 	private:
-		std::map<const std::shared_ptr<const CheckMethodLogin>, const handle_t> m_handles;
+		handles_t m_handles;
 };
