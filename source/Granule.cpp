5531064305be48620b9bb9d3f8ff339efd0b0aae
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-10 16:28:06 +0400

    [Add]: Начало реализации класса Granule

--- source/Granule.cpp
+++ source/Granule.cpp
@@ -2,8 +2,33 @@
 #include "stdafx.h"
 
 #include "Granule.h"
+#include "ProtectKey.h"
 
-Granule::Granule(void) {
+Granule::Granule(const std::string id, const std::shared_ptr<const ProtectKey> protect_key) :
+m_id(id),
+m_protect_key(protect_key)
+{
 }
 Granule::~Granule(void) {
+}
+
+const bool Granule::check(void) const {
+	bool result = false;
+	std::shared_ptr<const Granule> sp_this = shared_from_this();
+
+	for (const auto& element : m_check_methods) {
+		result = result && element->check(sp_this);
+		if (m_protect_key->is_key_nfr() && is_granule_nfr_date()) {
+			result = true;
+		}
+	}
+
+	return result;
+}
+
+void Granule::set_granule_nfr_date(bool is_granule_nfr_date) {
+	m_is_granule_nfr_date = is_granule_nfr_date;
+}
+const bool Granule::is_granule_nfr_date(void) const {
+	return m_is_granule_nfr_date;
 }
--- source/Granule.h
+++ source/Granule.h
@@ -3,8 +3,20 @@
 
 #include "KeyChecker.h"
 
-class Granule final : public KeyChecker {
+class ProtectKey;
+
+class Granule final : public KeyChecker, public std::enable_shared_from_this<const Granule> {
 	public:
-		Granule(void);
+		Granule(const std::string id, const std::shared_ptr<const ProtectKey> protect_key);
 		~Granule(void);
+	
+		virtual const bool check(void) const override final;
+
+		void set_granule_nfr_date(bool is_granule_nfr_date);
+		const bool is_granule_nfr_date(void) const;
+	protected:
+	private:
+		const std::string m_id;
+		const std::shared_ptr<const ProtectKey> m_protect_key;
+		bool m_is_granule_nfr_date;
 };
