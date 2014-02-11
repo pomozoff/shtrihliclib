3bd07217bcf5201959e27be7821a96ade02b06fb
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-11 12:55:08 +0400

    [Add]: Новый класс реализующий представление ключа HASP SL

---
+++ source/ProtectKeyHaspSL.cpp
@@ -0,0 +1,31 @@
+
+#include "stdafx.h"
+
+#include "ProtectKeyHaspSL.h"
+
+ProtectKeyHaspSL::ProtectKeyHaspSL() {
+}
+ProtectKeyHaspSL::~ProtectKeyHaspSL() {
+}
+
+/* ProtectKey Interface */
+const value_t ProtectKeyHaspSL::read_memory(const check_method_memory_t checkMethod) const {
+	value_t value;
+	return value;
+}
+
+/* IKeyChecker Interface */
+const bool ProtectKeyHaspSL::is_able_to_login(const check_method_login_t checkMethod) const {
+	return false;
+}
+const bool ProtectKeyHaspSL::is_same_memory(const check_method_memory_t checkMethod) const {
+	return false;
+}
+const bool ProtectKeyHaspSL::logout_key(const check_method_login_t checkMethod) const {
+	return false;
+}
+
+/* Private */
+void ProtectKeyHaspSL::free_licnese(void) const {
+
+}
---
+++ source/ProtectKeyHaspSL.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include "ProtectKey.h"
+
+class ProtectKeyHaspSL final : public ProtectKey {
+	public:
+		ProtectKeyHaspSL();
+		~ProtectKeyHaspSL();
+
+		/* ProtectKey Interface */
+		virtual const value_t read_memory(const check_method_memory_t checkMethod) const override;
+
+		/* IKeyChecker Interface */
+		virtual const bool is_able_to_login(const check_method_login_t checkMethod) const override;
+		virtual const bool is_same_memory(const check_method_memory_t checkMethod) const override;
+		virtual const bool logout_key(const check_method_login_t checkMethod) const override;
+	protected:
+		KeyType _key_type = KeyType::HaspSL;
+	private:
+		void free_licnese(void) const;
+};
