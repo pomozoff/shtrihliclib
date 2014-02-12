5cc145d183b09d1ed0192678994b9cd5cb5c5667
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-12 15:59:04 +0400

    [Ref]: Все определения типов убраны в отдельный заголовочный файл

--- source/CheckMethod.h
+++ source/CheckMethod.h
@@ -1,30 +1,9 @@
 ﻿
 #pragma once
 
-#include <vector>
-#include <memory>
+#include "DataTypes.h"
 
 enum class CheckMethodType { Base, Login, Memory };
-
-using byte_t    = uint8_t;
-using offset_t  = uint32_t;
-using feature_t = uint32_t;
-using handle_t  = uint32_t;
-
-using check_number_t = unsigned int;
-
-class IKeyChecker;
-class CheckMethod;
-class CheckMethodBase;
-class CheckMethodLogin;
-class CheckMethodMemory;
-
-using value_t = std::vector<const byte_t>;
-using ikey_checker_t = std::shared_ptr<const IKeyChecker>;
-using check_method_t = std::shared_ptr<const CheckMethod>;
-using check_method_base_t = std::shared_ptr<const CheckMethodBase>;
-using check_method_login_t = std::shared_ptr<const CheckMethodLogin>;
-using check_method_memory_t = std::shared_ptr<const CheckMethodMemory>;
 
 class CheckMethod {
 	public:
