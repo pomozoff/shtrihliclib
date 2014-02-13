30e855440669e345aae416af5017cf4904831453
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-13 10:27:41 +0400

    [Fix]: Добавлен забытый модификатор const

--- source/DataTypes.h
+++ source/DataTypes.h
@@ -26,4 +26,4 @@ using check_method_base_t = std::shared_ptr<const CheckMethodBase>;
 using check_method_login_t = std::shared_ptr<const CheckMethodLogin>;
 using check_method_memory_t = std::shared_ptr<const CheckMethodMemory>;
 
-using platform_t = std::shared_ptr<Platform>;
+using platform_t = std::shared_ptr<const Platform>;
