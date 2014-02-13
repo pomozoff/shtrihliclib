0ab5b8150bfc682f786361dff7f2c9f49f334f96
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-13 15:42:38 +0400

    [Add]: Добавлен метод установки значений любых типов в вектор

--- source/LicenseBlock.cpp
+++ source/LicenseBlock.cpp
@@ -1,6 +1,8 @@
 ﻿
 #include "stdafx.h"
 
+#include <iterator>
+
 #include "LicenseBlock.h"
 
 LicenseBlock::LicenseBlock(const value_t buffer, const offset_t offset, const time_t timeout) :
@@ -19,6 +21,19 @@ const value_t LicenseBlock::create_block_as_buffer_from_string(const std::string
 }
 
 /* Private */
+template <typename T>
+const bool LicenseBlock::place_data_to_buffer_at_offset(const T data, value_t& buffer, const offset_t offset) {
+	if (offset >= buffer.size() || (offset + sizeof data) > buffer.size()) {
+		return false;
+	}
+
+	auto begin = static_cast<const byte_t*>(static_cast<const void*>(&data));
+	auto end   = static_cast<const byte_t*>(static_cast<const void*>(&data)) + sizeof data;
+
+	std::copy(begin, end, &buffer[offset]);
+
+	return true;
+}
 const value_t LicenseBlock::create_block_as_buffer_from_hash(const size_t hash, const time_t time_logged_in) {
 	value_t buffer(sizeof_data);
 
--- source/LicenseBlock.h
+++ source/LicenseBlock.h
@@ -18,6 +18,9 @@ class LicenseBlock final {
 		static const size_t sizeof_data = sizeof_hash + sizeof(time_t);
 
 		LicenseBlock(const value_t buffer, const offset_t offset, const time_t timeout);
+
+		template <typename T>
+		static const bool place_data_to_buffer_at_offset(const T data, value_t& buffer, const offset_t offset);
 		static const value_t create_block_as_buffer_from_hash(const size_t hash, const time_t time_logged_in);
 		static const size_t hash_value(value_t buffer);
 };
