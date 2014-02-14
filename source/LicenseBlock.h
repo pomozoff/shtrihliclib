4e0822349bc976ca4b82942e33d5d08fd0b79f0a
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-14 16:05:20 +0400

    [Fix]: Определение приватны констант перенесено в правильное место

--- source/LicenseBlock.h
+++ source/LicenseBlock.h
@@ -4,20 +4,19 @@
 #include "DataTypes.h"
 
 class LicenseBlock final {
+	private:
+		static const size_t sizeof_hash = sizeof(size_t);
+		static const size_t sizeof_data = sizeof_hash + sizeof(time_t);
 	public:
 		~LicenseBlock(void);
 
 		static const size_t sizeof_block = sizeof_data + sizeof_hash;
-
 		static const value_t create_block_as_buffer_from_string(const std::string session_id, const time_t time_logged_in);
 	protected:
 	private:
 		const value_t _buffer;
 		const offset_t _offset;
 		const time_t _timeout;
-
-		static const size_t sizeof_hash = sizeof(size_t);
-		static const size_t sizeof_data = sizeof_hash + sizeof(time_t);
 
 		LicenseBlock(const value_t buffer, const offset_t offset, const time_t timeout);
 
