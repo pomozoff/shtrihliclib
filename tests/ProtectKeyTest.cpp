45798fceea7bb5d0016219721996284c103944c7
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-14 16:05:55 +0400

    [Add]: Доделан тест по копированию блока в буфер

--- tests/ProtectKeyTest.cpp
+++ tests/ProtectKeyTest.cpp
@@ -12,8 +12,17 @@ ProtectKeyTest::~ProtectKeyTest() {
 }
 
 TEST_F(ProtectKeyTest, copy_block_to_buffer) {
-	const value_t block = LicenseBlock::create_block_as_buffer_from_string(R"(computer-user)", time(NULL));
+	byte_t first_byte = 124;
+	byte_t last_byte = 235;
+	time_t some_time = 1392379203;
+	const value_t block = LicenseBlock::create_block_as_buffer_from_string(R"(computer-user)", some_time);
+	ASSERT_EQ(block[0], first_byte);
+	ASSERT_EQ(block[LicenseBlock::sizeof_block - 1], last_byte);
+
+	offset_t block_offset = LicenseBlock::sizeof_block * 4;
 	value_t buffer(ProtectKeyHaspSL::ReadWriteMemorySize);
-	
-	ProtectKey::copy_block_to_buffer(block, buffer, sizeof block, 0, LicenseBlock::sizeof_block * 4);
+	bool successs_copy = ProtectKey::copy_block_to_buffer(block, buffer, sizeof block, 0, block_offset);
+	ASSERT_TRUE(successs_copy);
+	ASSERT_EQ(buffer[block_offset], first_byte);
+	ASSERT_EQ(buffer[block_offset + LicenseBlock::sizeof_block - 1], last_byte);
 }
