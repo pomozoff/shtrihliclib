8a3fdc5b61b6ee3eefae2994694d3728e1f815ae
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-14 13:06:51 +0400

    [Add]: Новый тестовый класс

---
+++ tests/ProtectKeyTest.cpp
@@ -0,0 +1,19 @@
+
+#include "stdafx.h"
+
+#include "ProtectKeyTest.h"
+#include "ProtectKeyHaspSL.h"
+#include "LicenseBlock.h"
+
+ProtectKeyTest::ProtectKeyTest() {
+	_protect_key_hasp_sl = ProtectKey::create_key(KeyType::HaspSL);
+}
+ProtectKeyTest::~ProtectKeyTest() {
+}
+
+TEST_F(ProtectKeyTest, copy_block_to_buffer) {
+	const value_t block = LicenseBlock::create_block_as_buffer_from_string(R"(computer-user)", time(NULL));
+	value_t buffer(ProtectKeyHaspSL::ReadWriteMemorySize);
+	
+	ProtectKey::copy_block_to_buffer(block, buffer, sizeof block, 0, LicenseBlock::sizeof_block * 4);
+}
---
+++ tests/ProtectKeyTest.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+#include "gtest\gtest.h"
+#include "ProtectKey.h"
+
+class ProtectKeyTest : public ::testing::Test {
+	public:
+		ProtectKeyTest();
+		~ProtectKeyTest();
+	protected:
+		protect_key_t _protect_key_hasp_sl;
+};
