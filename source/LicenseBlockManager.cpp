cab3661624904ebc8383fd78cf5812ec9c505db8
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-21 15:50:58 +0400

    [Mod]: Сделан конструктор менеджера блоков лицензий

--- source/LicenseBlockManager.cpp
+++ source/LicenseBlockManager.cpp
@@ -3,7 +3,11 @@
 
 #include "LicenseBlockManager.h"
 
-LicenseBlockManager::LicenseBlockManager(void) {
+LicenseBlockManager::LicenseBlockManager(const value_t buffer, const time_t timeout, const size_t licenses_amount) :
+_timeout(timeout),
+_licenses_amount(licenses_amount),
+_license_blocks(license_blocks_from_buffer(buffer))
+{
 }
 LicenseBlockManager::~LicenseBlockManager(void) {
 }
--- source/LicenseBlockManager.h
+++ source/LicenseBlockManager.h
@@ -1,8 +1,14 @@
 ﻿
 #pragma once
 
+#include "DataTypes.h"
+
 class LicenseBlockManager final {
 	public:
-		LicenseBlockManager(void);
+		LicenseBlockManager(const value_t buffer, const time_t timeout, const size_t licenses_amount);
 		~LicenseBlockManager(void);
+	private:
+		const license_blocks_t _license_blocks;
+		const time_t _timeout;
+		const size_t _licenses_amount;
 };
