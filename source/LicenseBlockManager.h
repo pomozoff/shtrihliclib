0d0449e2f2fbf63af10ad333e12f3adf16cf38d6
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-21 18:35:36 +0400

    [Mod]: Убран модификатор const для изменяемого поля класса

--- source/LicenseBlockManager.h
+++ source/LicenseBlockManager.h
@@ -8,6 +8,6 @@ class LicenseBlockManager final {
 		LicenseBlockManager(const value_t buffer, const time_t timeout, const size_t licenses_amount);
 		~LicenseBlockManager(void);
 	private:
-		const license_blocks_t _license_blocks;
+		license_blocks_t _license_blocks;
 		const size_t _licenses_amount;
 };
