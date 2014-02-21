b0a3a808f49a12f82065dd7034a2e8ba000c7569
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-21 15:49:39 +0400

    [Add]: Добавлены новые типы для работы с блоками лицензий

--- source/DataTypes.h
+++ source/DataTypes.h
@@ -18,6 +18,7 @@ class CheckMethodBase;
 class CheckMethodLogin;
 class CheckMethodMemory;
 class Platform;
+class LicenseBlock;
 
 using value_t = std::vector<const byte_t>;
 using ikey_checker_t = std::shared_ptr<const IKeyChecker>;
@@ -26,3 +27,5 @@ using check_method_base_t = std::shared_ptr<const CheckMethodBase>;
 using check_method_login_t = std::shared_ptr<const CheckMethodLogin>;
 using check_method_memory_t = std::shared_ptr<const CheckMethodMemory>;
 using platform_t = std::shared_ptr<const Platform>;
+using license_block_t = std::shared_ptr<LicenseBlock>;
+using license_blocks_t = std::vector<const license_block_t>;
