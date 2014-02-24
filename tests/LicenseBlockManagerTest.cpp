0eaafbc6c1a942890900f93e9252298c916760d3
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-24 18:24:09 +0400

    [Add]: Добавлен новый тест поиска свободной лицензии

---
+++ tests/LicenseBlockManagerTest.cpp
@@ -0,0 +1,23 @@
+
+#include "stdafx.h"
+
+#include "LicenseBlockManagerTest.h"
+
+LicenseBlockManagerTest::LicenseBlockManagerTest() {
+}
+LicenseBlockManagerTest::~LicenseBlockManagerTest() {
+}
+
+TEST_F(LicenseBlockManagerTest, take_license) {
+	const size_t sizeof_buffer = 4096;
+	const time_t timeout = 30;
+	const size_t licenses_amount = 2;
+
+	value_t buffer(sizeof_buffer);
+	std::fill(buffer.begin(), buffer.end(), 0);
+
+	auto manager = std::make_shared<const LicenseBlockManager>(buffer, timeout, licenses_amount);
+	auto license_block = manager->take_license();
+
+	ASSERT_TRUE(nullptr != license_block);
+}
---
+++ tests/LicenseBlockManagerTest.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+#include "gtest\gtest.h"
+#include "LicenseBlockManager.h"
+
+class LicenseBlockManagerTest : public ::testing::Test {
+	public:
+		LicenseBlockManagerTest();
+		~LicenseBlockManagerTest();
+};
