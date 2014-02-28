e83f8f8406b79c25b69a91e591839618cb38ab11
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-28 16:40:38 +0400

    [Add]: Добавлен виртуальный метод did_check_protect_key()

--- source/IProtectKeyDelegate.h
+++ source/IProtectKeyDelegate.h
@@ -4,5 +4,8 @@
 class IProtectKeyDelegate {
 	public:
 		IProtectKeyDelegate(void);
+
+		virtual void did_check_protect_key(bool success) const = 0;
+	protected:
 		virtual ~IProtectKeyDelegate(void);
 };
