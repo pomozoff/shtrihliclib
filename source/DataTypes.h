60f75b3abca81b50ccf5b072dedae295bfd6a4ff
Anton Pomozov <pomozov@shtrih-m.ru>
2014-02-28 14:15:21 +0400

    [Add]: Добавлены новые типы

--- source/DataTypes.h
+++ source/DataTypes.h
@@ -9,6 +9,7 @@ using byte_t = uint8_t;
 using offset_t = uint32_t;
 using feature_t = uint32_t;
 using handle_t = uint32_t;
+using features_t = std::vector<const feature_t>;
 
 using check_number_t = unsigned int;
 
@@ -19,6 +20,7 @@ class CheckMethodLogin;
 class CheckMethodMemory;
 class Platform;
 class LicenseBlock;
+class LicenseBlockManager;
 
 using value_t = std::vector<const byte_t>;
 using ikey_checker_t = std::shared_ptr<const IKeyChecker>;
@@ -29,3 +31,4 @@ using check_method_memory_t = std::shared_ptr<const CheckMethodMemory>;
 using platform_t = std::shared_ptr<const Platform>;
 using license_block_t = std::shared_ptr<const LicenseBlock>;
 using license_blocks_t = std::vector<const license_block_t>;
+using license_block_manager_t = std::shared_ptr<const LicenseBlockManager>;
