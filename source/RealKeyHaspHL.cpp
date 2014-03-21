269356f0c10a12c0d02cb33e572d980eef596177
Anton Pomozov <pomozov@shtrih-m.ru>
2014-03-21 16:48:03 +0400

    [Add]: Добавлена реализация работы с реальным ключом Hasp HL

---
+++ source/RealKeyHaspHL.cpp
@@ -0,0 +1,11 @@
+
+#include "stdafx.h"
+
+#include "RealKeyHaspHL.h"
+
+#pragma region Constructor Destructor
+RealKeyHaspHL::RealKeyHaspHL(void) {
+}
+RealKeyHaspHL::~RealKeyHaspHL(void) {
+}
+#pragma endregion Constructor Destructor
