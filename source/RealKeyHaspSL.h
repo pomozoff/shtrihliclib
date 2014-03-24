5b40377a52340f6c030daedd47d49ac8c0ba4d39
Anton Pomozov <pomozov@shtrih-m.ru>
2014-03-24 17:58:58 +0400

    [Ref]: Для осуществления тестирования убраны модификаторы final у виртуальных методов классов

--- source/RealKeyHaspSL.h
+++ source/RealKeyHaspSL.h
@@ -9,10 +9,9 @@ class RealKeyHaspSL : public IRealKeyHasp {
 		virtual ~RealKeyHaspSL(void);
 
 		/* IProtectKey IRealKeyHasp */
-		virtual const hasp_status_t _hasp_login_scope(const hasp_feature_t feature_id, const char* scope, const hasp_vendor_code_t vendor_code, hasp_handle_t& handle) const override final;
-		virtual const hasp_status_t _hasp_read(const hasp_handle_t handle, const size_t file_id, const hasp_size_t offset, const int length, value_t& buffer) const override final;
-		virtual const hasp_status_t _hasp_write(const hasp_handle_t handle, const size_t file_id, const hasp_size_t offset, const int length, const value_t& buffer) const override final;
-		virtual const hasp_status_t _hasp_logout(const hasp_handle_t handle) const  override final;
-	protected:
+		virtual const hasp_status_t _hasp_login_scope(const hasp_feature_t feature_id, const char* scope, const hasp_vendor_code_t vendor_code, hasp_handle_t& handle) const override;
+		virtual const hasp_status_t _hasp_read(const hasp_handle_t handle, const size_t file_id, const hasp_size_t offset, const int length, value_t& buffer) const override;
+		virtual const hasp_status_t _hasp_write(const hasp_handle_t handle, const size_t file_id, const hasp_size_t offset, const int length, const value_t& buffer) const override;
+		virtual const hasp_status_t _hasp_logout(const hasp_handle_t handle) const override;
 		virtual const hasp_status_t _hasp_legacy_set_idletime(const hasp_handle_t handle, const hasp_u16_t idle_time) const override;
 };
