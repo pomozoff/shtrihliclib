76f398b7668fb2c89d5186c2edf005ce80d62096
Anton Pomozov <pomozov@shtrih-m.ru>
2014-03-25 14:35:57 +0400

    [Mod]: Косметические изменения

--- tests/ProtectKeyTest.cpp
+++ tests/ProtectKeyTest.cpp
@@ -15,16 +15,19 @@ ProtectKeyTest::~ProtectKeyTest(void) {
 
 #pragma region Tests
 TEST_F(ProtectKeyTest, copy_block_to_buffer) {
+	std::string id = R"(computer-user)";
 	byte_t first_byte = 124;
 	byte_t last_byte = 235;
 	time_t some_time = 1392379203;
-	const value_t block = LicenseBlock::block_from_string(R"(computer-user)", some_time);
+	const value_t block = LicenseBlock::block_from_string(id, some_time);
+
 	ASSERT_EQ(block[0], first_byte);
 	ASSERT_EQ(block[LicenseBlock::sizeof_block - 1], last_byte);
 
 	offset_t block_offset = LicenseBlock::sizeof_block * 4;
 	value_t buffer(ProtectKeyHaspSL::read_write_memory_size);
 	bool successs_copy = ProtectKey::copy_block_to_buffer(block, buffer, sizeof block, 0, block_offset);
+
 	ASSERT_TRUE(successs_copy);
 	ASSERT_EQ(buffer[block_offset], first_byte);
 	ASSERT_EQ(buffer[block_offset + LicenseBlock::sizeof_block - 1], last_byte);
