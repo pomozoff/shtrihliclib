3612e9c41d81e96cbbcdd99480137d54af5b2a6f
Anton Pomozov <pomozov@shtrih-m.ru>
2014-04-14 15:51:08 +0400

    [Add]: Добавлена реализация получения имени компьютера и текущего пользователя для Windows

--- source/PlatformWindows.cpp
+++ source/PlatformWindows.cpp
@@ -1,6 +1,12 @@
 
 #include "stdafx.h"
 
+#define SECURITY_WIN32
+
+#include <Windows.h>
+#include <security.h>
+#include <secext.h>
+
 #include "PlatformWindows.h"
 
 #pragma region Constructor Destructor
@@ -14,7 +20,27 @@ PlatformWindows::~PlatformWindows(void) {
 
 #pragma region Private
 const std::wstring PlatformWindows::get_computer_name(void) const {
+	DWORD size = 0;
+	GetComputerNameExW(ComputerNameNetBIOS, NULL, &size);
+
+	PTCHAR buffer = new TCHAR[size];
+	std::wstring computerName = L"";
+	if (GetComputerNameExW(ComputerNameNetBIOS, buffer, &size)) {
+		computerName = buffer;
+	}
+	delete[] buffer;
+	return computerName;
 }
 const std::wstring PlatformWindows::get_user_name(void) const {
+	DWORD size = 0;
+	GetUserNameExW(NameDisplay, NULL, &size);
+
+	PTCHAR buffer = new TCHAR[size];
+	std::wstring userName = L"";
+	if (GetUserNameExW(NameSamCompatible, buffer, &size)) {
+		userName = buffer;
+	}
+	delete[] buffer;
+	return userName;
 }
 #pragma endregion
