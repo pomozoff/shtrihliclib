143c91b39c4ee14382553e15ef90e28bbc963ad7
Anton Pomozov <pomozov@shtrih-m.ru>
2014-04-14 15:31:06 +0400

    [Ref]: Добавлены модификаторы const для метода did_check_protect_key()

--- source/IProtectKeyDelegate.h
+++ source/IProtectKeyDelegate.h
@@ -5,7 +5,7 @@ class IProtectKeyDelegate {
 	public:
 		IProtectKeyDelegate(void);
 
-		virtual void did_check_protect_key(bool success) const = 0;
+		virtual void did_check_protect_key(const bool success) const = 0;
 	protected:
 		virtual ~IProtectKeyDelegate(void);
 };
