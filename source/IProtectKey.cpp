640c609ceb7e01554094674fc7a9eacbaea11fbc
Anton Pomozov <pomozov@shtrih-m.ru>
2014-04-15 15:22:45 +0400

    [Add]: Новое поле метода проверки типа логин для декодирования буфера

--- source/IProtectKey.cpp
+++ source/IProtectKey.cpp
@@ -9,3 +9,9 @@ IProtectKey::IProtectKey(void) {
 IProtectKey::~IProtectKey(void) {
 }
 #pragma endregion
+
+#pragma region Accessors
+void IProtectKey::set_decrypt_method(const check_method_login_t decrypt_method) const {
+	_decrypt_method = decrypt_method;
+}
+#pragma endregion
--- source/IProtectKey.h
+++ source/IProtectKey.h
@@ -1,6 +1,10 @@
 
 #pragma once
 
+#include <stdint.h>
+
+#include "DataTypes.h"
+
 class IProtectKey {
 	public:
 		virtual ~IProtectKey(void);
@@ -8,6 +12,11 @@ class IProtectKey {
 		virtual const bool check_license(void) const = 0;
 		virtual const bool is_key_nfr(void) const = 0;
 		virtual const bool is_key_base(void) const = 0;
+		virtual const bool decrypt(uint8_t* buffer, const size_t length) const = 0;
+
+		void set_decrypt_method(const check_method_login_t decrypt_method) const;
 	protected:
 		IProtectKey(void);
+
+		mutable check_method_login_t _decrypt_method = nullptr;
 };
