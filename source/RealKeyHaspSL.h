f6438039f3b7ff62d7ad73ae9bf8f52cd908af70
Anton Pomozov <pomozov@shtrih-m.ru>
2014-04-15 15:23:16 +0400

    [Mod]: Уточнён комментарий

--- source/RealKeyHaspSL.h
+++ source/RealKeyHaspSL.h
@@ -8,7 +8,7 @@ class RealKeyHaspSL : public IRealKeyHasp {
 		RealKeyHaspSL(void);
 		virtual ~RealKeyHaspSL(void);
 
-		/* IProtectKey IRealKeyHasp */
+		/* IRealKeyHasp Interface */
 		virtual const hasp_status_t _hasp_login_scope(const hasp_feature_t feature_id, const char* scope, const hasp_vendor_code_t vendor_code, hasp_handle_t& handle) const override;
 		virtual const hasp_status_t _hasp_read(const hasp_handle_t handle, const hasp_fileid_t file_id, const hasp_size_t offset, const int length, value_t& buffer) const override;
 		virtual const hasp_status_t _hasp_write(const hasp_handle_t handle, const hasp_fileid_t file_id, const hasp_size_t offset, const int length, const value_t& buffer) const override;
