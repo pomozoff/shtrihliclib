05f68fd24b0c7af0498e45dde394f3de77cddd11
Anton Pomozov <pomozov@shtrih-m.ru>
2014-04-25 11:55:39 +0400

    [Ref]: Добавлен модификатор const

--- source/IProtectKeyDelegate.h
+++ source/IProtectKeyDelegate.h
@@ -7,7 +7,7 @@ class IProtectKeyDelegate {
 	public:
 		IProtectKeyDelegate(void);
 
-		virtual void did_check_protect_key(const bool success) const = 0;
+		virtual void did_check_protect_key(const bool success) = 0;
 	protected:
 		virtual ~IProtectKeyDelegate(void);
 };
