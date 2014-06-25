28508f736412eeced11be716fbb7e61341dd686c
Anton Pomozov <pomozov@shtrih-m.ru>
2014-06-25 19:31:05 +0400

    [Add]: Добавлен класс тестов для гранул

---
+++ tests/GranuleTest.cpp
@@ -0,0 +1,17 @@
+
+#include "stdafx.h"
+
+#include "GranuleTest.h"
+
+#pragma region Constructor Destructor
+GranuleTest::GranuleTest(void) {
+}
+GranuleTest::~GranuleTest(void) {
+}
+#pragma endregion
+
+#pragma region Tests
+TEST_F(GranuleTest, granule_has_name) {
+	ASSERT_TRUE(false);
+}
+#pragma endregion
---
+++ tests/GranuleTest.h
@@ -0,0 +1,13 @@
+
+#ifndef __GRANULETEST_H__
+#define __GRANULETEST_H__
+
+#include "gtest/gtest.h"
+
+class GranuleTest : public ::testing::Test {
+	public:
+		GranuleTest(void);
+		virtual ~GranuleTest(void);
+};
+
+#endif // __GRANULETEST_H__
