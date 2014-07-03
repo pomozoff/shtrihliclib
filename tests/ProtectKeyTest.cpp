b8035c7abb9067b8f877085ad21c98c268d470bc
Anton Pomozov <pomozov@shtrih-m.ru>
2014-07-03 13:08:44 +0400

    [Add]: Добавлен тест на поиск одного HaspSL ключа

--- tests/ProtectKeyTest.cpp
+++ tests/ProtectKeyTest.cpp
@@ -2,6 +2,10 @@
 #include "stdafx.h"
 
 #include "ProtectKeyTest.h"
+#include "ProtectKeyHaspSLTest.h"
+#include "Platform.h"
+#include "CheckMethodLogin.h"
+#include "CheckMethodMemory.h"
 
 #pragma region Constructor Destructor
 ProtectKeyTest::ProtectKeyTest(void) {
@@ -11,6 +15,26 @@ ProtectKeyTest::~ProtectKeyTest(void) {
 #pragma endregion
 
 #pragma region Tests
-TEST_F(ProtectKeyTest, decrypt) {
+TEST_F(ProtectKeyTest, hasp_sl_key) {
+	auto protect_keys = createKeysOneHaspSL();
+	auto iprotect_key_weak = ProtectKey::find_key(protect_keys, *this);
+	ASSERT_TRUE(nullptr != iprotect_key_weak.lock());
+}
+#pragma endregion
+
+#pragma region Protecetd
+const protect_keys_t ProtectKeyTest::createKeysOneHaspSL(void) const {
+	auto protectKey = ProtectKeyHaspSLTest::create_hasp_sl_key(_feature_hasp_sl, Platform::platform()->session_id(), _licenses_amount_two);
+	const auto checkMethod = protectKey->create_check_method_login(_feature_hasp_sl, false);
+	checkMethod->set_check_method_for_license(true);
+	checkMethod->set_logout_after_check(true);
+
+	protect_keys_t protect_keys;
+	protect_keys.push_back(protectKey);
+
+	return protect_keys;
+}
+void ProtectKeyTest::did_check_protect_key(const bool success) {
+	ASSERT_TRUE(success);
 }
 #pragma endregion
--- tests/ProtectKeyTest.h
+++ tests/ProtectKeyTest.h
@@ -4,11 +4,21 @@
 
 #include "gtest/gtest.h"
 
-class ProtectKeyTest : public ::testing::Test {
+#include "ProtectKey.h"
+#include "IProtectKeyDelegate.h"
+
+class ProtectKeyTest : public ::testing::Test, public IProtectKeyDelegate {
 	public:
 		ProtectKeyTest(void);
 		~ProtectKeyTest(void);
 	protected:
+		const protect_keys_t createKeysOneHaspSL(void) const;
+	private:
+		const feature_t _feature_hasp_sl = 3;
+		const std::wstring _session_id = L"(computer-username)";
+		const size_t _licenses_amount_two = 2;
+
+		virtual void did_check_protect_key(const bool success) override final;
 };
 
 #endif // __PROTECTKEYTEST_H__
